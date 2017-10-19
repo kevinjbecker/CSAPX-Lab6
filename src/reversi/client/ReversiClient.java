370df8075a069eb148d6b8070c9c315e690ceaf1
Kevin <kjb2503@rit.edu>
2017-10-19 16:29:50 -0400

    Implemented some of the client-side programming.
    ---------- "Lighthouse" by anonymous ----------
    Flames alight in midnight seas
    Beckon souls to destinies
    Among the others wrecked before
    But one, who sailed onto your shore
    Addled whisps of yearning for
    The one who calms your sea.

--- src/reversi/client/ReversiClient.java
+++ src/reversi/client/ReversiClient.java
@@ -1,11 +1,64 @@
 package reversi.client;
 
-import reversi.Reversi;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
-public class ReversiClient
+import reversi.*;
+import java.net.Socket;
+
+public class ReversiClient implements ReversiProtocol
 {
     public static void main(String [] args)
     {
+        Reversi clientGame = null;
 
+        if(args.length != 2)
+        {
+            System.out.println("Invalid number of arguments.\nUsage: java ReversiClient host port");
+        }
+        else
+        {
+            try(
+                Socket conn = new Socket(args[0], Integer.parseInt(args[1]))
+            )
+            {
+                BufferedReader connIn = new BufferedReader( new InputStreamReader( conn.getInputStream() ) );
+                PrintWriter connOut = new PrintWriter( conn.getOutputStream(), true );
+
+                //As soon as it connects it should immediately create the game so we can do that here
+                String response = connIn.readLine();
+                System.out.println("Client got request: " + response);
+
+
+                String [] initialCommand = response.split(" ");
+
+                // Constructs the client game
+                clientGame = new Reversi(Integer.parseInt(initialCommand[1]), Integer.parseInt(initialCommand[2]));
+
+                response = connIn.readLine();
+
+                if(response.equals(MAKE_MOVE))
+                {
+                    connOut.println(MOVE + " " + 3 + " " + 2);
+                }
+
+                response = connIn.readLine();
+
+                System.out.println("Client got request: " + response);
+
+
+                /*
+                while(response.equals())
+                {
+
+                }
+                */
+            }
+            catch (java.io.IOException ioe)
+            {
+                System.out.println("An error has occurred while attempting to run the game.");
+            }
+        }
     }
 }
--- src/reversi/server/ReversiServer.java
+++ src/reversi/server/ReversiServer.java
@@ -3,13 +3,17 @@ import reversi.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 import java.io.IOException;
-import java.net.Socket;
 
 public class ReversiServer implements ReversiProtocol
 {
+
+    public static int numMoves = 0;
+
     public static void main(String [] args)
     {
         int numRows, numCols, port;
@@ -24,13 +28,13 @@ public class ReversiServer implements ReversiProtocol
             numCols = Integer.parseInt(args[1]);
             port = Integer.parseInt(args[2]);
 
-            gameIO(numRows, numCols, port);
+            gameIO(port, numRows, numCols);
         }
     }
 
-    private static void gameIO(int numRows, int numCols, int port)
+    private static void gameIO(int port, int numRows, int numCols)
     {
-        Reversi game = new Reversi(numRows, numCols);
+        Reversi serverGame = new Reversi(numRows, numCols);
 
         ServerSocket server = null;
         Socket conn1 = null;
@@ -39,24 +43,56 @@ public class ReversiServer implements ReversiProtocol
         try
         {
             // Alerts the start of the program.
-            System.out.println("Server is being initialized...");
-            // Initializes the server on port.
+            //System.out.println("Server is being initialized on port "+ port + " ...");
+            // Initializes the servers on the port.
             server = new ServerSocket(port);
             // Alerts the user what port the server is on.
-            System.out.println("Server initialized on port" + port + ".");
+            //System.out.println("Server initialization complete.");
 
-            // Allows for player 1 to connect to server.
-            System.out.print("Waiting for player 1 to connect...");
+            // Allows for player 1 to connect to server1.
+            System.out.print("Waiting for player 1 to connect... ");
+
             conn1 = server.accept();
-            System.out.println("Player 1 connected.");
+            BufferedReader conn1In = new BufferedReader( new InputStreamReader( conn1.getInputStream() ) );
+            PrintWriter conn1Out = new PrintWriter( conn1.getOutputStream(), true );
 
-            // Allows for player 2 to connect to server.
-            System.out.print("Waiting for player 2 to connect...");
+            // We tell conn1 it was connected then the numRows and numCols
+            conn1Out.println(CONNECT + " " + numRows + " " + numCols);
+
+            System.out.println("connected!");
+
+            // Allows for player 2 to connect to server2.
+            System.out.print("Waiting for player 2 to connect... ");
+
             conn2 = server.accept();
-            System.out.println("Player 2 connected.\nGame is starting!");
+            BufferedReader conn2In = new BufferedReader( new InputStreamReader( conn2.getInputStream() ) );
+            PrintWriter conn2Out = new PrintWriter( conn2.getOutputStream(), true );
 
-            // Starts running the game.
+            // We tell conn2 it was connected then the numRows and numCols
+            conn2Out.println(CONNECT + " " + numRows + " " + numCols);
 
+            System.out.println("connected!\nGame is starting!");
+
+            conn1Out.println(MAKE_MOVE);
+            String move1 = conn1In.readLine();
+            System.out.println(move1);
+
+            conn2Out.println(MAKE_MOVE);
+            String move2 = conn2In.readLine();
+            System.out.println(move2);
+
+            conn1Out.println(GAME_LOST);
+            conn2Out.println(GAME_WON);
+
+
+            /*
+            while(!serverGame.gameOver())
+            {
+                // Adds one to the number of moves made
+                ++numMoves;
+
+            }
+            */
         }
         catch (IOException gameRunIOE)
         {
