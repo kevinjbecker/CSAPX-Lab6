525d8ae84f551ccb9457a83023fd3ca84e6e5255
Kevin <kjb2503@rit.edu>
2017-10-23 11:49:15 -0400

    Did a bit of code-rearrangement and also commented the hell out of everything! Now it is easy to tell which line of code is doing what. It's super effective.
    Because I have now used a term often seen in Pokémon, here are the
    entire lyrics to the television series about Pokémon.
    
    I wanna be the very best
    Like no one ever was
    To catch them is my real test
    To train them is my cause
    I will travel across the land
    Searching far and wide
    Each Pokémon to understand
    The power that's inside
    Pokémon, (gotta catch them all) its you and me
    I know its my destiny
    Pokemon, oh, you're my best friend
    In a world we must defend
    Pokémon, (gotta catch them all) a heart so true
    Our courage will pull us through
    You teach me and I'll teach you
    (Po-ké-mon) Gotta catch 'em all
    Gotta catch 'em all
    Every challenge along the way
    With courage I will face
    I will battle every day
    To claim my rightful place
    Come with me, the time is right
    There's no better team
    Arm in arm we'll win the fight
    It's always been our dream
    Pokémon! (Gotta catch 'em all)
    It's you and me
    I know it's my destiny
    Pokémon!
    Oh, you're my best friend
    In a world we must defend
    Pokémon! (Gotta catch 'em all)
    A heart so true
    Our courage will pull us through
    You teach me and I'll teach you
    Pokémon!
    Gotta catch 'em all
    Gotta catch 'em all
    Gotta catch 'em all
    Gotta catch 'em all
    Gotta catch 'em all
    Pokémon! (Gotta catch 'em all)
    It's you and me
    I know it's my destiny
    Pokémon!
    Oh, you're my best friend
    In a world we must defend
    Pokémon! (Gotta catch 'em all)
    A heart so true
    Our courage will pull us through
    You teach me and I'll teach you
    POKÉMON!
    Gotta catch'em all!!
    Gotta catch'em all!!
    POKÉMON!!!

--- src/reversi/client/ReversiClient.java
+++ src/reversi/client/ReversiClient.java
@@ -1,88 +1,133 @@
 package reversi.client;
 
+import reversi.*;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.IOException;
 
-import reversi.*;
 import java.net.Socket;
+
 import java.util.Scanner;
 
+
+/**
+ * The ReversiClient is used by an end-user and allows them to play the game of Reversi with another player.
+ *
+ * @author Kevin Becker
+ */
 public class ReversiClient implements ReversiProtocol
 {
+    /**
+     * Begins execution of the Reversi game between a server and a client.
+     * @param args The arguments that are used for the creation of a connection.
+     *             The arguments should have the following:<br><br><em>
+     *             0 => The location where the host can be found.<br>
+     *             1 => The port where the host can be found.</em>
+     */
     public static void main(String [] args)
     {
-        Reversi clientGame;
-        String [] serverMessage;
-
+        // if the number of arguments is not strictly 2, we exit because it could cause an issue.
         if(args.length != 2)
         {
             System.out.println("Invalid number of arguments.\nUsage: java ReversiClient host port");
             System.exit(1);
         }
 
+        // runs the game if we have completed our check and it passes
+        runGame(args[0], Integer.parseInt(args[1]));
+    }
+
+    /**
+     * Runs the actual game and communications between the client and server.
+     * @param host The host of the ServerSocket where the ReversiServer is found
+     * @param port The port of the ServerSocket where the ReversiServer is found
+     */
+    private static void runGame(String host, int port)
+    {
+        // creates a clientGame (used to show the board after each move)
+        Reversi clientGame;
+        // used for the messages received by the server
+        String [] message;
+        // continueRunning is used for the loop so that we know whether we need to stop or not
+        // this method is preferred because we only stop once the server tells us the game is over (either through
+        // ERROR or with the game result
+        boolean continueRunning = true;
+
+        // try-with-resources creating a connection, communication pipelines to and from the server, and a userIn
         try(
-                Socket conn = new Socket(args[0], Integer.parseInt(args[1]));
-                Scanner userIn = new Scanner(System.in)
+                Socket conn = new Socket(host, port);
+                BufferedReader connIn = new BufferedReader( new InputStreamReader( conn.getInputStream() ) );
+                PrintWriter connOut = new PrintWriter( conn.getOutputStream(), true );
+                Scanner playerIn = new Scanner(System.in)
         )
         {
-            BufferedReader connIn = new BufferedReader( new InputStreamReader( conn.getInputStream() ) );
-            PrintWriter connOut = new PrintWriter( conn.getOutputStream(), true );
+            // as soon as it connects it should immediately create the game so we can do that here
+            message = connIn.readLine().split(" ");
 
-            //As soon as it connects it should immediately create the game so we can do that here
-            serverMessage = connIn.readLine().split(" ");
+            // constructs the client game using the server's given message
+            clientGame = new Reversi(Integer.parseInt(message[1]), Integer.parseInt(message[2]));
 
-            // Constructs the client game
-            clientGame = new Reversi(Integer.parseInt(serverMessage[1]), Integer.parseInt(serverMessage[2]));
-
-            //Prints the initial game
+            // prints the initial game to the screen
             System.out.println(clientGame);
-            boolean continueRunning = true;
 
             while(continueRunning)
             {
-                serverMessage = connIn.readLine().split(" ");
-                switch (serverMessage[0])
+                // reads in the server's message and splits it by spaces
+                message = connIn.readLine().split(" ");
+                // has a switch on the keyword (the first index in the message array)
+                switch (message[0])
                 {
+                    // if the message is MAKE_MOVE, prompt the user to make a move and send it to the server
+                    // NO UPDATE IS MADE TO THE CLIENT GAME, WE ONLY MODIFY OUR VERSION WHEN THE SERVER TELLS US TO
                     case MAKE_MOVE:
                         System.out.print("It is your turn to move! Enter row column: ");
-                        connOut.println(MOVE + " " + userIn.nextLine());
+                        connOut.println(MOVE + " " + playerIn.nextLine());
                         break;
+                    // if the message is MOVE_MADE we tell the user what move was made and then update our game
                     case MOVE_MADE:
-                        System.out.println("A move has been made in row: " + serverMessage[1] + " column: " + serverMessage[2]);
-                        clientGame.makeMove(Integer.parseInt(serverMessage[1]), Integer.parseInt(serverMessage[2]));
+                        System.out.println("A move has been made in row: " + message[1] + " column: " + message[2]);
+                        clientGame.makeMove(Integer.parseInt(message[1]), Integer.parseInt(message[2]));
                         System.out.println(clientGame);
                         break;
+                    // if the message is GAME_WON, tell the user they won and then exit the loop
                     case GAME_WON:
                         System.out.println("The game is over. You won!");
                         continueRunning = false;
                         break;
+                    // if the message is GAME_LOST, tell the user they lost and then exit the loop
                     case GAME_LOST:
                         System.out.println("The game is over. You lost!");
                         continueRunning = false;
                         break;
+                    // if the message is GAME_TIED, tell the user they tied and then exit the loop
                     case GAME_TIED:
                         System.out.println("The game is over. You tied!");
                         continueRunning = false;
                         break;
+                    // if the message is ERROR, alert the user that the server hit an issue an that the game is exiting
                     case ERROR:
                         System.out.println("The server hit an issue. The game will now exit.");
                         continueRunning = false;
                 }
             }
         }
-        catch (java.io.IOException ioe)
+        // if we catch an IOException on client side, we do that here
+        catch (IOException ioe)
         {
-            System.out.println("An error has occurred while attempting to run the game.");
+            System.err.println("An error has occurred while attempting to run the game. The client will now halt.");
             System.out.println(ioe.getMessage());
         }
+        // if we catch a NumberFormatException (i.e. the server port is bad) we tell the user here.
         catch(NumberFormatException ne)
         {
-            System.err.println("Host port must be an integer 0-65535.");
+            System.err.println("Host port must be an integer 0-65535. The client will now halt.");
         }
+        // if we catch a ReversiException (somehow), we alert the user and exit the game.
         catch (ReversiException re)
         {
-            System.out.println("We should never get here but the server has made an error... Quitting.");
+            System.err.println("We should never get here but the server has hit an error. The client will now halt.");
         }
     }
 }
--- src/reversi/server/ReversiServer.java
+++ src/reversi/server/ReversiServer.java
@@ -1,162 +1,225 @@
 package reversi.server;
+
 import reversi.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.IOException;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import java.io.IOException;
 
+/**
+ * A server which can run a game of Reversi, a flip-flop game played with two players. ReversiServer controls the
+ * interactions between two players (whom are using ReversiClient).
+ *
+ * @author Kevin Becker
+ */
 public class ReversiServer implements ReversiProtocol
 {
-
-    private static int numMoves = 0;
-
+    /**
+     * The main method that checks correct
+     * @param args The arguments that are used for the creation of a server and Reversi board.
+     *             The arguments should have the following:<br><br><em>
+     *             0 => The number of rows the Reversi board should have.<br>
+     *             1 => The number of columns the Reversi board should have.<br>
+     *             2 => The port to which the server should be created.</em>
+     */
     public static void main(String [] args)
     {
         int numRows, numCols, port;
 
-        if(args.length < 3)
+        // if we don't have enough arguments, we don't try to run because that would just be bad
+        // alerts that there was an issue and exits with code 1
+        if(args.length != 3)
         {
             System.out.println("Invalid number of arguments.\nUsage: java ReversiServer #_rows #_cols port");
             System.exit(1);
         }
 
+        // sets numRows to its integer value
         numRows = Integer.parseInt(args[0]);
+        // sets numCols to its integer value
         numCols = Integer.parseInt(args[1]);
+        // sets port to its integer value
         port = Integer.parseInt(args[2]);
 
-        gameIO(port, numRows, numCols);
+        // sends control to the gameIO method to construct the server and get all of the players set up
+        gameIO(numRows, numCols, port);
     }
 
-    private static void gameIO(int port, int numRows, int numCols)
+    /**
+     * Builds the server and then accepts two clients.
+     * @param numRows The number of rows in the Reversi game.
+     * @param numCols The number of columns in the Reversi game.
+     * @param port The port the server should be located on.
+     */
+    private static void gameIO(int numRows, int numCols, int port)
     {
+        // creates a server version of the
         Reversi serverGame = new Reversi(numRows, numCols);
 
+        // creates a null ServerSocket used for the server
         ServerSocket server = null;
 
-        Socket conn1 = null;
-        Socket conn2 = null;
+        // creates null Sockets used for the two clients
+        Socket player1 = null;
+        Socket player2 = null;
 
-        BufferedReader conn1In;
-        PrintWriter conn1Out = null;
-        BufferedReader conn2In;
-        PrintWriter conn2Out = null;
+        // creates BufferedReaders for each of the players
+        BufferedReader player1In;
+        BufferedReader player2In;
 
-        String [] move;
+        // creates a PrintWriter for each of the players
+        PrintWriter player1Out = null;
+        PrintWriter player2Out = null;
 
         try
         {
-            // Alerts the start of the program.
-            //System.out.println("Server is being initialized on port "+ port + " ...");
-            // Initializes the servers on the port.
+            // initializes the servers on the port
             server = new ServerSocket(port);
-            // Alerts the user what port the server is on.
-            //System.out.println("Server initialization complete.");
 
-            // Allows for player 1 to connect to server1.
+            // waits for player one to connect to server
             System.out.print("Waiting for player 1 to connect... ");
+            player1 = server.accept();
+            // creates a BufferedReader and PrintWriter for the player1 communication
+            player1In = new BufferedReader(new InputStreamReader(player1.getInputStream()));
+            player1Out = new PrintWriter(player1.getOutputStream(), true);
 
-            conn1 = server.accept();
-            conn1In = new BufferedReader(new InputStreamReader(conn1.getInputStream()));
-            conn1Out = new PrintWriter(conn1.getOutputStream(), true);
+            // we tell player1 it connected successfully, then tell it the number of rows and columns in the game
+            player1Out.println(CONNECT + " " + numRows + " " + numCols);
 
-            // We tell conn1 it was connected then the numRows and numCols
-            conn1Out.println(CONNECT + " " + numRows + " " + numCols);
-
+            // once player1 is fully connected we output this
             System.out.println("connected!");
 
-            // Allows for player 2 to connect to server2.
+            // waits for player two to connect to server2
             System.out.print("Waiting for player 2 to connect... ");
+            player2 = server.accept();
+            // once player2 is fully connected we output this
+            player2In = new BufferedReader(new InputStreamReader(player2.getInputStream()));
+            player2Out = new PrintWriter(player2.getOutputStream(), true);
 
-            conn2 = server.accept();
-            conn2In = new BufferedReader(new InputStreamReader(conn2.getInputStream()));
-            conn2Out = new PrintWriter(conn2.getOutputStream(), true);
+            // We tell player2 it was connected successfully then tell it the number of rows and columns in the game
+            player2Out.println(CONNECT + " " + numRows + " " + numCols);
 
-            // We tell conn2 it was connected then the numRows and numCols
-            conn2Out.println(CONNECT + " " + numRows + " " + numCols);
-
+            // once player2 is fully connected we output this
             System.out.println("connected!\nGame is starting!");
 
-            while (!serverGame.gameOver())
-            {
-
-                if (numMoves % 2 == 0)
-                {
-                    conn1Out.println(MAKE_MOVE);
-                    move = conn1In.readLine().split(" ");
-                }
-                else
-                {
-                    conn2Out.println(MAKE_MOVE);
-                    move = conn2In.readLine().split(" ");
-                }
-
-                // attempts to make the move here
-                serverGame.makeMove(Integer.parseInt(move[1]), Integer.parseInt(move[2]));
-                // if there was no exception thrown we can tell each client the move was okay so they can
-                // update their versions of the game.
-                conn1Out.println(MOVE_MADE + " " + move[1] + " " + move[2]);
-                conn2Out.println(MOVE_MADE + " " + move[1] + " " + move[2]);
-
-                // DEBUGGING ONLY
-                System.out.println("A move has been made. Master game reads: ");
-                System.out.println(serverGame);
-
-                ++numMoves;
-            }
-
-            Reversi.Move winner = serverGame.getWinner();
-
-            if(winner.equals(Reversi.Move.PLAYER_ONE))
-            {
-                conn1Out.println(GAME_WON);
-                conn2Out.println(GAME_LOST);
-            }
-            else if (winner.equals(Reversi.Move.PLAYER_TWO))
-            {
-                conn2Out.println(GAME_WON);
-                conn1Out.println(GAME_LOST);
-            }
-            else
-            {
-                conn1Out.println(GAME_TIED);
-                conn2Out.println(GAME_TIED);
-            }
+            // runs the game using the created items
+            runGame(serverGame, player1In, player1Out, player2In, player2Out);
         }
         catch (IOException gameRunIOE)
         {
-            System.out.println("An error has occurred while attempting to run the game.");
+            // look into this
+            System.out.println("An error has occurred in communication. Halting server.");
             System.out.println(gameRunIOE.getMessage());
             gameRunIOE.printStackTrace();
         }
         catch(ReversiException re)
         {
-            System.out.println("Reversi hit an issue. Halting clients.");
-            conn1Out.println(ERROR);
-            conn2Out.println(ERROR);
+            // do the actions if we hit a ReversiException
+            System.out.println("An error has occurred in Reversi (possibly with a move requested). Halting server and clients.");
+            player1Out.println(ERROR);
+            player2Out.println(ERROR);
         }
         finally
         {
+            // after the try, no matter what, we try to close the connections and the server socket if they're not already so
             try
             {
-                // Closing server1 if it's not already closed.
+                // closing server1 if it's not already closed
                 if (server != null) server.close();
 
-                // Closing conn1 if it's not already closed.
-                if (conn1 != null) conn1.close();
+                // closing conn1 if it's not already closed
+                if (player1 != null) player1.close();
 
-                // Closing conn2 if it's not already closed.
-                if (conn2 != null) conn2.close();
+                // closing conn2 if it's not already closed
+                if (player2 != null) player2.close();
             }
             catch (IOException socketCloseIOE)
             {
-                System.out.println("An error occurred while closing sockets.");
+                // if we catch an IOException here, we explode
+                System.out.println("An error occurred while closing sockets. Halting server.");
                 System.out.println(socketCloseIOE.getMessage());
                 socketCloseIOE.printStackTrace();
             }
+        }
+    }
+
+    /**
+     * This method runs the game logic. Keeps track of whose turn it is and the moves being made.
+     * @param serverGame The server's version of the ReversiGame that is considered the "master"
+     * @param player1In Player one's BufferedReader pipeline (allows player1 to send to the server)
+     * @param player1Out Player one's PrintWriter pipeline (allows the server to send to player1)
+     * @param player2In Player two's BufferedReader pipeline (allows player1 to send to the server)
+     * @param player2Out Player two's PrintWriter pipeline (allows the server to send to player1)
+     * @throws ReversiException An exception thrown if there was an issue in the ReversiGame. Usually occurs when a bad
+     * move has been requested (a space taken already occupied)
+     * @throws IOException An exception thrown if there was an issue communicating with Sockets (in both directions)
+     */
+    private static void runGame(Reversi serverGame, BufferedReader player1In, PrintWriter player1Out,
+                                BufferedReader player2In, PrintWriter player2Out) throws ReversiException, IOException
+    {
+        // used to tell whose move it is
+        int numMoves = 0;
+        // used to hold the client response on each pass of the loop
+        String [] message;
+
+        // continues looping until the game is over (as specified by the Reversi class)
+        while (!serverGame.gameOver())
+        {
+            // if numMoves is even, it is player one's turn
+            if (numMoves % 2 == 0)
+            {
+                // tells player1 it's their turn
+                player1Out.println(MAKE_MOVE);
+                // reads their response and splits it by spaces
+                message = player1In.readLine().split(" ");
+            }
+            // else it is player two's turn
+            else
+            {
+                // tells player2 it's their turn
+                player2Out.println(MAKE_MOVE);
+                // reads their response and splits it by spaces
+                message = player2In.readLine().split(" ");
+            }
+
+            // attempts to make the move requested by the client (held in move)
+            // if it fails, the method throws a ReversiException to the gameIO method
+            serverGame.makeMove(Integer.parseInt(message[1]), Integer.parseInt(message[2]));
+
+            // if there was no exception thrown we can tell each client the move was okay so they can
+            // update their copies of the game
+            player1Out.println(MOVE_MADE + " " + message[1] + " " + message[2]);
+            player2Out.println(MOVE_MADE + " " + message[1] + " " + message[2]);
+
+            // increment numMoves for the next turn
+            ++numMoves;
+        }
+
+        // once we exit the loop we get the winner
+        Reversi.Move winner = serverGame.getWinner();
+
+        // if the winner is player one, do the associated actions
+        if(winner.equals(Reversi.Move.PLAYER_ONE))
+        {
+            player1Out.println(GAME_WON);
+            player2Out.println(GAME_LOST);
+        }
+        // else if the winner is player two, do the associated actions
+        else if (winner.equals(Reversi.Move.PLAYER_TWO))
+        {
+            player2Out.println(GAME_WON);
+            player1Out.println(GAME_LOST);
+        }
+        // otherwise it was a tie
+        else
+        {
+            player1Out.println(GAME_TIED);
+            player2Out.println(GAME_TIED);
         }
     }
 }
